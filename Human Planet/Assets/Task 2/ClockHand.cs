e585ea666391cfc0eec3298008cc0b1743e31182
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2020-05-14 11:16:40 +0300

    task 2 is accomplished

---
+++ Human Planet/Assets/Task 2/ClockHand.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ClockHand : MonoBehaviour
+{
+    [SerializeField] Transform hourHand;
+    [SerializeField] Transform minuteHand;
+    [SerializeField] Transform secondHand;
+
+    private float seconds = 360/60f;
+    private float hour = 360 / 12f;
+    // Update is called once per frame
+    void Update()
+    {
+        //DateTime time = DateTime.Now;
+        //secondHand.localRotation = Quaternion.Euler(0, 0, -time.Second*seconds);
+        //minuteHand.localRotation = Quaternion.Euler(0, 0, -time.Minute * seconds);
+        //hourHand.localRotation = Quaternion.Euler(0, 0, -time.Hour * hour);
+        TimeSpan timespan = DateTime.Now.TimeOfDay;
+        hourHand.localRotation =
+            Quaternion.Euler(0f, 0f, (float)timespan.TotalHours * -hour);
+        minuteHand.localRotation =
+            Quaternion.Euler(0f, 0f, (float)timespan.TotalMinutes * -seconds);
+        secondHand.localRotation =
+            Quaternion.Euler(0f, 0f, (float)timespan.TotalSeconds * -seconds);
+    }
+}
---
+++ Human Planet/Assets/Task 2/OnClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        Color newColor=new Color();
+        newColor.r = Random.Range(0f, 1f);
+        newColor.g = Random.Range(0f, 1f);
+        newColor.b = Random.Range(0f, 1f);
+        newColor.a = 1f;
+
+        spriteRenderer.color = newColor;
+    }
+}
